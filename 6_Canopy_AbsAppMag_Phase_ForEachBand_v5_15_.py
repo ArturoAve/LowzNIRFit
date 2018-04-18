8064abbd804cfec012fc0532fb40f30df2e94bd3
Arturo Avelino <aavelino@cfa.harvard.edu>
2018-04-18 18:35:26 -0400

    Remove ipynb checkpoint
    Remove ipynb checkpoint

--- 6_Canopy_AbsAppMag_Phase_ForEachBand_v5_15_.py
+++ 6_Canopy_AbsAppMag_Phase_ForEachBand_v5_15_.py
@@ -1,35 +1,35 @@
 
 # coding: utf-8
 
-# # Create text files of: 
+# # Create text files of:
 # # - (phase, absolute mag, error mag) and
 # # - (phase, apparent mag, error mag)
 
 # ### Notes
-# 
+#
 # - The section of the code more pulished is for J band:  use it as template for any other band.
 # - So far, the only case that apply (z_min, z_max) are the bands: J
 # - So far, the only case that apply (z_min) are the bands: B, J
 
 # # User interface
 
-# In[1]:
+# In[ ]:
 
 # USER
 
 import sys # To read arguments in command line
 
-#--- For command line ---
-# bandName = '%s'%sys.argv[1]
-# Sample = '%s'%sys.argv[2]
- 
-#---- For notebook----
-bandName = 'J'  # (Y, Y, H, K)
+# For command line
+bandName = '%s'%sys.argv[1]
+Sample = '%s'%sys.argv[2]
 
-Sample = 'CfA'
-# Sample = 'CSP' 
-# Sample = 'Others' 
-# Sample = 'CfA_CSP' 
+# For notebook:
+# bandName = 'J'
+
+# Sample = 'CfA'
+# Sample = 'CSP'
+# Sample = 'Others'
+# Sample = 'CfA_CSP'
 
 
 #----------------
@@ -46,30 +46,30 @@ wFix = -1
 
 # # Automatic
 
-# In[2]:
+# In[ ]:
 
 from snpy import *
-# from numpy import * 
+# from numpy import *
 import numpy as np
 from matplotlib import pyplot as plt
 import json
 import glob # To read the name of the files in a given directory
 
-# %pylab qt 
+# %pylab qt
 # For CANOPY python: to show the plots in a separated Windows instead of inline.
 # If used, then I don't have to put the instruction "plt.close()" at
-# the end of the plot because it will show the figure and instantaneously 
+# the end of the plot because it will show the figure and instantaneously
 # close the windows too.
 
 5+6
 
 
-# In[2]:
+# In[ ]:
 
 
 
 
-# In[3]:
+# In[ ]:
 
 
 #-------- BAND --------------------
@@ -86,7 +86,7 @@ if bandName == 'H':
 if bandName == 'K':
     filterNames = ['Ks2m', 'KANDI', 'K', 'Kd']
 
-#-------------------- 
+#--------------------
 
 DataType = 'OpticalNIR' # OpticalNIR, Optical
 
@@ -104,7 +104,7 @@ FilterSyst = 'Std_filters/'
 # FilterSyst = 'CSP_filters/'
 
 
-# In[4]:
+# In[ ]:
 
 
 # Range of redshift data to be considered
@@ -142,14 +142,14 @@ cc = 299792.458  # Speed of light (km/s)
 # MainDir = '/Users/arturo/Dropbox/Research/SoftwareResearch/Snoopy/AndyLCComp_2018_02/'
 
 
-# In[4]:
+# In[ ]:
 
 
 
 
 # ## Metadata: (zhel, zcmb)
 
-# In[5]:
+# In[ ]:
 
 DirMetadata = '/Users/arturo/Dropbox/Research/SoftwareResearch/Snoopy/AndyLCComp_2018_02/'
 
@@ -159,16 +159,16 @@ infoSNe_data = np.genfromtxt(DirMetadata+
                             dtype=['S17', float,float, 'S40',float,float,
                                    float,float,float,float,'S16', int ])
 
-# Create a dictionary: 
+# Create a dictionary:
 # {snname: ra, dec, zhelio, e_zhel, zcmb, e_zcmb, zcmbFlow, e_zcmbFlow, code}
 
 infoSNe_dict = {infoSNe_data['f0'][i]: np.array( [ infoSNe_data['f1'][i],
-                infoSNe_data['f2'][i], 
+                infoSNe_data['f2'][i],
                 infoSNe_data['f4'][i]/cc, infoSNe_data['f5'][i]/cc,
                 infoSNe_data['f6'][i]/cc, infoSNe_data['f7'][i]/cc,
                 infoSNe_data['f8'][i]/cc, infoSNe_data['f9'][i]/cc,
                 infoSNe_data['f11'][i]] )
-                for i in range(len(infoSNe_data)) } 
+                for i in range(len(infoSNe_data)) }
 
 print infoSNe_dict['sn1991T']
 # [  1.88542500e+02   2.66556000e+00   5.79067269e-03   3.33564095e-06
@@ -176,21 +176,21 @@ print infoSNe_dict['sn1991T']
 #    2.00000000e+00]
 
 
-# In[5]:
+# In[ ]:
 
 
 
 
 # ### Uploading the SED of Hsiao to compute the extinction
 
-# In[6]:
+# In[ ]:
 
 Ia_w, Ia_f = kcorr.get_SED(0,'H3')
 
 
 # ### Cosmology theory for $\mu(z)$
 
-# In[7]:
+# In[ ]:
 
 from scipy.integrate import quad as intquad
 
@@ -204,13 +204,13 @@ def InvEHubblePar(z, OmM, wde):
 def LumDistance(z, OmM, wde, Ho):
     "Luminosity distance"
     LumDistanceVecInt = 0.
-    LumDistanceVecInt = cc*(1.+z)*intquad(InvEHubblePar, 0., z, args=(OmM, wde))[0]/Ho 
+    LumDistanceVecInt = cc*(1.+z)*intquad(InvEHubblePar, 0., z, args=(OmM, wde))[0]/Ho
     return LumDistanceVecInt
 
 
 # ---- Distance modulus ----
 def DistanceMu(z, OmM, wde, Ho):
-    "Distance modulus"     
+    "Distance modulus"
     DistanceMuInt = 5.0*np.log10(LumDistance(z, OmM, wde, Ho)) + 25.0
     return DistanceMuInt
 
@@ -222,24 +222,24 @@ print DistanceMu(ztest1, OmMFix, wFix, HoFix)
 # 38.2202031449
 
 
-# In[7]:
+# In[ ]:
 
 
 
 
 # ### Generating a table of data (phase[TBmax], M, error_M) including the info of the SNe, from the "snpy" files
-# 
+#
 # The output data is already corrected by
 # - k-correction
 # - extinction by Milky Way dust
 # - time dilation using the z_helio
-# 
+#
 
-# In[8]:
+# In[ ]:
 
 #- Directory to save the data
 
-DirSaveFiles = '/Users/arturo/Dropbox/Research/Articulos/10_AndyKaisey/10Compute/TheTemplates/'+bandName+'_band/'+FilterSyst+'1_AllData_InitialFit/AbsMag/'+Sample+'/'    
+DirSaveFiles = '/Users/arturo/Dropbox/Research/Articulos/10_AndyKaisey/10Compute/TheTemplates/'+bandName+'_band/'+FilterSyst+'1_AllData_InitialFit/AbsMag/'+Sample+'/'
 
 #- Force the creation of the directory to save the outputs.
 #- "If the subdirectory does not exist then create it"
@@ -251,19 +251,19 @@ DirLocationData = '/Users/arturo/Dropbox/Research/SoftwareResearch/Snoopy/AndyLC
 
 os.chdir(DirLocationData)
 
-# Create a list of the the ".snpy" file names: 
+# Create a list of the the ".snpy" file names:
 list_SN = glob.glob('*_StdFilt.snpy')
 # tmp. list_SN = glob.glob('*_1stFit.snpy') # tmp
 
 print len(list_SN),"SNe read with extension '_StdFilt.snpy.'"
 
 
-# In[8]:
+# In[ ]:
 
 
 
 
-# In[9]:
+# In[ ]:
 
 # AUTOMATIC: THIS DOES NOT NEED USER INTERACTION
 
@@ -289,16 +289,16 @@ countSN_nir = 0 # Count SNe with data in a given NIR band
 for sn in list_SN: # Loop over the SNe.
 
     s=get_sn(sn) # upload .snpy info
-    
+
     # The kcorr uncertainties were computed during the snoopy fitting
     # of the light curves?
-    if error_kcorr == True:    
+    if error_kcorr == True:
         # upload the .json dictionary containing the k-corr uncertainties
-        with open(sn[:-12]+'kcorr_Mean_Std.json','r') as outfile: s_errorKcorr = json.load(outfile)  
-            
+        with open(sn[:-12]+'kcorr_Mean_Std.json','r') as outfile: s_errorKcorr = json.load(outfile)
+
     zhelio     = infoSNe_dict[s.name][2]
     err_zhelio = infoSNe_dict[s.name][3]
-    
+
     # Flag to determine the appropiate z_cmb:
     flag_zcmb  = infoSNe_dict[s.name][8]
     if flag_zcmb > 0.1:
@@ -312,22 +312,22 @@ for sn in list_SN: # Loop over the SNe.
 
         # Count the number of similar bands in the same SNe (e.g., J, J2m)
         CountNumSimilarBands = 0
-        
+
         # Loop over all the variants of filters names in a given band.
-        for band in filterNames: 
+        for band in filterNames:
             if (band in s.data.keys() and
                 (s.data[band].MJD[0] -s.Tmax)/(1+zhelio) <= phase_min and
                 (s.data[band].MJD[-1]-s.Tmax)/(1+zhelio) >= phase_max and
                 len(s.data[band].MJD) >= MinNumberOfDataInLCs):
 
                 CountNumSimilarBands = CountNumSimilarBands + 1
-                    
-                if CountNumSimilarBands == 1: 
+
+                if CountNumSimilarBands == 1:
                     SN_txtfile = open(DirSaveFiles+sn[0:18]+'_'+Sample+'_'+
                                       bandName+'.txt', 'w')
                 else:
                     SN_txtfile = open(DirSaveFiles+sn[0:18]+'_'+Sample+'_'+
-                                      bandName+'.txt', 'a')    
+                                      bandName+'.txt', 'a')
                     SN_txtfile.write("#------ %s ------\n"%band)
 
                 #------- The extinction A_lambda --------
@@ -420,7 +420,7 @@ Err_appmag   kcorr    err_kcorr  \n')
                               s.data[band].e_mag[i] == flag2)):
                             CommentText = '##'
                         else: CommentText = ''
-                            
+
                     # Define the err_kcorr value:
                     if error_kcorr == True:
                             err_kcorr = s_errorKcorr[band]['%s'%s.data[band].MJD[i]][1]
@@ -488,7 +488,7 @@ print len(list_SN),"# SNe read with extension '_StdFilt.snpy.'"
 
 # In[ ]:
 
-# -- 69 SNe in subsample CfA with data in J band: done -- 
+# -- 69 SNe in subsample CfA with data in J band: done --
 # -- 84 SNe in subsample CSP with data in J band: done --
 
 
